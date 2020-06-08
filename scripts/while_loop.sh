ae7829c6c4404a54376894006750a96f946ab2dc
papatki <patrycja.p9428@gmail.com>
2020-06-08 18:47:13 +0200

    update while.loop.sh

--- scripts/while_loop.sh
+++ scripts/while_loop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-LOG_FILE=logfile.log
+LOG_FILE=/home/patrycja/log.txt
 max_retry=10
 i=1
 while true
@@ -7,5 +7,5 @@ do
 [[ $i -eq max_retry ]] && echo "Last attempt-sending email notification" >> ${LOG_FILE} && echo -e "Subject: 10th attempt `hostname`\nTo: patrycja.p9428@gmail.com\n\n`cat ${LOG_FILE}`" | /usr/sbin/sendmail patrycja.p9428@gmail.com && exit 1
 echo "#$i attempt" >> ${LOG_FILE}
 ((i++))
-sleep 5
+sleep 1
 done
