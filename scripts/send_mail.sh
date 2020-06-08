4aa856654bb3e5f32c6af586763e1fc163e33580
papatki <patrycja.p9428@gmail.com>
2020-06-08 19:08:17 +0200

    rm mail address

--- scripts/send_mail.sh
+++ scripts/send_mail.sh
@@ -4,7 +4,7 @@ max_retry=10
 i=1
 while true
 do
-[[ $i -eq max_retry ]] && echo "Last attempt-send email with attachment" >> ${LOG_FILE} && echo "Message Body" | mutt -s "Subject" -a log.txt -- patrycja.p9428@gmail.com && exit 1
+[[ $i -eq max_retry ]] && echo "Last attempt-send email with attachment" >> ${LOG_FILE} && echo "Message Body" | mutt -s "Subject" -a log.txt -- email@mail.com && exit 1
 echo "#$i attempt" >> ${LOG_FILE}
 ((i++))
 sleep 1
