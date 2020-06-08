e325cbdcc508cbb5077514fc104d953c3fe21892
papatki <patrycja.p9428@gmail.com>
2020-06-08 18:51:04 +0200

    add send_mail.sh

---
+++ scripts/send_mail.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+LOG_FILE=/home/patrycja/log.txt
+max_retry=10
+i=1
+while true
+do
+[[ $i -eq max_retry ]] && echo "Last attempt-send email with attachment" >> ${LOG_FILE} && echo "Message Body" | mutt -s "Subject" -a log.txt -- patrycja.p9428@gmail.com && exit 1
+echo "#$i attempt" >> ${LOG_FILE}
+((i++))
+sleep 1
+done
