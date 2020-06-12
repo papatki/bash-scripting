4ca6fc9e9e4ae7f8ae9e86fc595d3639f46a3d41
papatki <patrycja.p9428@gmail.com>
2020-06-12 08:59:10 +0200

    update case_stat.sh

--- scripts/case_stat.sh
+++ scripts/case_stat.sh
@@ -3,15 +3,9 @@
 #This script demonstrates the case statement
 
 case "${1}" in
-	start)
-		echo 'Starting'
-		;;
-	stop)
-		echo 'Stopping'
-		;;
-	status|state|--status|--state)
-		echo 'Status'
-		;;
+	start) echo 'Starting' ;;
+	stop) echo 'Stopping' ;;
+	status) echo 'Status' ;;
 	*)
 		echo 'Supply a valid option.' >&2
 		exit 1
