6e5f05f2a7ce42d35156ca090e0b7dd8d1350055
papatki <patrycja.p9428@gmail.com>
2020-06-12 08:29:44 +0200

    add STDOUT and STDERR redirections

--- scripts/add-local-user-new2.sh
+++ scripts/add-local-user-new2.sh
@@ -8,15 +8,16 @@
 # check if the script is being executed with superuser privileges
 if [[ "${UID}" -ne 0 ]]
 then
-	echo 'Please run with sudo or as root'
+	#redirect output to the STDERR
+	echo 'Please run with sudo or as root' >&2
 	exit 1
 fi
 
 # if they don't supply at least one argument, give help
 if [[ "${#}" -lt 1 ]]
 then
-	echo "Usage: ${0} USER_NAME [COMMENT]..."
-	echo 'Create an account on the local system with the name of USER_NAME and COMMENT'
+	echo "Usage: ${0} USER_NAME [COMMENT]..." >&2
+	echo 'Create an account on the local system with the name of USER_NAME and COMMENT' >&2
 	exit 1
 fi
 
@@ -31,30 +32,29 @@ COMMENT="${@}"
 PASSWORD=$(date +%s%N | sha265sum | head -c12)
 
 # create user with the password
-useradd -c "{COMMENT}" -m ${USER_NAME}
+useradd -c "{COMMENT}" -m ${USER_NAME} &> /dev/null
 
 # check if useradd command succeeded
 if [[ "${?}" -ne 0 ]]
 then
-	echo 'The account could not be created'
+	echo 'The account could not be created' >&2
 	exit 1
 fi
 
 # set the password
-echo ${PASSWORD} | passwd --stdin ${USER_NAME}
+echo ${PASSWORD} | passwd --stdin ${USER_NAME} &> /dev/null
 
 # check if passwd command succeede
 if [[ "${?}" -ne 0 ]]
 then
-	echo 'The password could not be set'
+	echo 'The password could not be set' >&2
 	exit 1
 fi
 
 # force password change on first login
-passwd -e ${USER_NAME}
+passwd -e ${USER_NAME} &> /dev/null
 
 # display info about user
-echo
 echo "Username: ${USER_NAME}"
 echo "Password: ${PASSWORD}"
 echo "Host: ${HOSTNAME}"
